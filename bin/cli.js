d695fd78715026ab037e420e8e19828af2520f6f
hpk992 <jkuang41@gmail.com>
2023-02-18 11:16:23 -0800

    cli git update

--- bin/cli.js
+++ bin/cli.js
@@ -3,30 +3,33 @@
 const { execSync } = require('child_process');
 
 const runCommand = (command) => {
-  try {
-    execSync(`${command}`, { stdio: 'inherit' });
-  } catch (error) {
-    console.error(`Failed to execute ${command}`, e);
-    return false;
-  }
-  return true;
+    try {
+        execSync(`${command}`, { stdio: 'inherit' });
+    } catch (error) {
+        console.error(`Failed to execute ${command}`, e);
+        return false;
+    }
+    return true;
 };
 
 const repoName = process.argv[2];
 
 const gitCloneCommand = `git clone --depth 1 https://github.com/startercode-dev/js-template.git ${repoName}`;
-const removeGitCommand = `rm -rf ./${repoName}/.git`;
-const installDepsCommand = `cd ${repoName} && npm i && git init`;
+const installDepsCommand = `cd ${repoName} && npm i`;
+const localGitCommand = `cd ${repoName} && rm -rf ./.git && git init && git add . && git commit -m 'init'`;
 
 console.log(`Cloning template into ${repoName}`);
 const gitClone = runCommand(gitCloneCommand);
 if (!gitClone) process.exit(1);
-const removeGit = runCommand(removeGitCommand);
+
+console.log('Setting up local git...');
+const localGit = runCommand(localGitCommand);
+if (!localGit) process.exit(1);
 
 console.log(`Installing dependencies for ${repoName}`);
 const installDeps = runCommand(installDepsCommand);
 if (!installDeps) process.exit(1);
 
-console.log(`---------------------`);
+console.log(`----------------------`);
 console.log(`ALL DONE! =]`);
 console.log(`'cd ${repoName}' to get started!`);
